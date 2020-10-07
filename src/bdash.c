2ad04a58572c65f2e4a09b28005b34c8c0d524cb
mazaczysko <miloszrygula@interia.pl>
2020-10-07 11:39:58 +0200

    Add basic map and player structures, basic map rendering

--- src/bdash.c
+++ src/bdash.c
@@ -12,6 +12,47 @@ typedef enum
 	TILE_PLAYER
 }tile_id;
 
+typedef struct
+{
+	int x, y;
+	ALLEGRO_BITMAP *tile;
+}player;
+
+typedef struct
+{
+	int width, height;
+	ALLEGRO_BITMAP **tile;
+}map;
+
+
+void player_init( player *player, ALLEGRO_BITMAP *tile, int x, int y )
+{
+	player->tile = tile;
+	player->x = x;
+	player->y = y;
+}
+
+void map_init( map *map, int width, int height )
+{
+	map->width = width;
+	map->height = height;
+	map->tile = calloc( width * height, sizeof( ALLEGRO_BITMAP *) );
+}
+
+ALLEGRO_BITMAP *map_get_tile( map *map, int x, int y )
+{
+	if( x < 0 || y < 0 || x > map->width || y > map->height ) return NULL;
+	return map->tile[x+y*map->width]; 
+}
+
+int map_put_tile( map *map, int x, int y, ALLEGRO_BITMAP *tile )
+{
+	if( x < 0 || y < 0 || x > map->width || y > map->height ) return 1;
+	
+	map->tile[x+y*map->width] = tile;
+	return 0;
+}
+
 int main( int argc, char **argv )
 {
 	//Init allegro
@@ -22,7 +63,7 @@ int main( int argc, char **argv )
 	ALLEGRO_DISPLAY *win = al_create_display( 800, 600 );
 	ALLEGRO_FONT *font = al_create_builtin_font( );
 
-	ALLEGRO_TIMER *draw_timer = al_create_timer( 1.0 / 30.0 );
+	ALLEGRO_TIMER *draw_timer = al_create_timer( 1.0 / 60.0 );
 	ALLEGRO_EVENT_QUEUE *queue = al_create_event_queue( );
 	ALLEGRO_EVENT ev;
 
@@ -41,7 +82,18 @@ int main( int argc, char **argv )
 		[TILE_PLAYER] = al_load_bitmap( "resources/player.png" )
 	};
 
-	int alive = 1, draw = 0;
+	int alive = 1;
+
+	player player;
+	player_init( &player, tiles[TILE_PLAYER], 5, 5 );
+
+	map map;
+	map_init( &map, 20, 20 );
+
+	ALLEGRO_BITMAP *t;
+
+	map_put_tile( &map, player.x, player.y, player.tile );
+	
 
 	while( alive )
 	{
@@ -56,35 +108,32 @@ int main( int argc, char **argv )
 					break;
 
 				case ALLEGRO_EVENT_KEY_DOWN:
-					draw = 1;
 					break;
 
 				case ALLEGRO_EVENT_TIMER:
 					if( ev.timer.source == draw_timer )
 					{
-						if( !draw )
-						{
-							al_clear_to_color( al_map_rgb( 125, 16, 127 ) );
-							al_draw_text( font, al_map_rgb( 255, 255, 255 ), 0, 0, 0, "Hit your keyboard, come on!" );
+							al_clear_to_color( al_map_rgb( 0, 0, 0 ) );
+							for( int i = 0; i < map.width; i++ )
+							{
+								for( int j = 0; j < map.height; j++ )
+								{
+									t = map_get_tile( &map, i, j );
+									if( t != NULL )
+									{
+										al_draw_scaled_bitmap( t, 0, 0, 32, 32, i*64, j*64, 64, 64, 0 );
+									}
+								}
+							}	
+								
 							al_flip_display( );
-						}
-						else
-						{
-							// al_clear_to_color( al_map_rgb( 125, 40, 200 ) );
-							// al_draw_text( font, al_map_rgb( 255, 255, 255 ), 0, 0, 0, "Hopefully it works!" );
-							// al_draw_text( font, al_map_rgb( 255, 255, 255 ), 0, 10, 0, "Good job, but how do you close the window?" );
-							// al_flip_display( );
-							for( int i = 0; i < 5; i++ )
-								al_draw_scaled_bitmap( tiles[i], 0, 0, 32, 32, 50 + i*64, 100, 64, 64, 0 );
-							al_flip_display( );
-						}
 					}
 					break;
 
 				default:
 					break;
 			}
-		}while( al_get_next_event( queue, &ev ) );
+		}while( al_get_next_event( queue, &ev ) && alive );
 	}
 
 	for( int i = 0; i < 5; i++ )
